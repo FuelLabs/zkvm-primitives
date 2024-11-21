d44106573d4f3ebe28d9ab1f5c0fb825fd7c6468
rymnc <43716372+rymnc@users.noreply.github.com>
2024-11-21 18:09:01 +0530

    chore: add block with 1 other tx

--- fuel-zkvm-primitives-test-fixtures/src/mainnet_blocks.rs
+++ fuel-zkvm-primitives-test-fixtures/src/mainnet_blocks.rs
@@ -3,13 +3,18 @@
 pub enum MainnetBlocks {
     // This is an empty block with just a mint tx
     Block1522295,
+    // This is a block with a mint tx and a contract interaction with multiple assets
+    Block2243673,
 }
 
 pub fn get_mainnet_block_input(block: MainnetBlocks) -> Vec<u8> {
     match block {
         MainnetBlocks::Block1522295 => {
             include_bytes!("fixtures/mainnet_blocks/1522295.bin").to_vec()
-        }
+        },
+        MainnetBlocks::Block2243673 => {
+            include_bytes!("fixtures/mainnet_blocks/2243673.bin").to_vec()
+        },
     }
 }
 
@@ -23,6 +28,17 @@ mod tests {
     #[tokio::test]
     async fn test_mainnet_block_1522295() {
         let serialized_input = get_mainnet_block_input(MainnetBlocks::Block1522295);
+
+        let proof = prove(&serialized_input).unwrap();
+        let deserialized_input: Input = bincode::deserialize(&serialized_input).unwrap();
+        let block_id: [u8; 32] = deserialized_input.block.header().id().into();
+
+        assert_eq!(block_id, proof.block_id.to_be_bytes())
+    }
+
+    #[tokio::test]
+    async fn test_mainnet_block_2243673() {
+        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block2243673);
 
         let proof = prove(&serialized_input).unwrap();
         let deserialized_input: Input = bincode::deserialize(&serialized_input).unwrap();
