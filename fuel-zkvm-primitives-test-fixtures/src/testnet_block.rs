e41cef2e0f6f1df36f48cd0a527805484a684956
rymnc <43716372+rymnc@users.noreply.github.com>
2024-11-21 13:20:19 +0530

    fix: nits

--- fuel-zkvm-primitives-test-fixtures/src/testnet_block.rs
+++ fuel-zkvm-primitives-test-fixtures/src/testnet_block.rs
@@ -5,25 +5,15 @@ mod tests {
     use fuel_core::chain_config::{ChainConfig, SnapshotMetadata, StateConfig};
     use fuel_core::service::{Config, FuelService};
     use fuel_core::state::historical_rocksdb::StateRewindPolicy;
-    use fuel_core_types::fuel_tx::ConsensusParameters;
     use std::path::Path;
 
-    const CONSENSUS_PARAMETERS: &[u8] =
-        include_bytes!("fixtures/testnet_block/block_1360410_consensus_parameters.json");
-
     fn get_config(metadata_path: &Path, db_path: &Path) -> Config {
-        let mut chain_config =
+        let chain_config =
             ChainConfig::from_snapshot_metadata(&SnapshotMetadata::read(&metadata_path).unwrap())
                 .unwrap();
         let state_config =
             StateConfig::from_snapshot_metadata(SnapshotMetadata::read(&metadata_path).unwrap())
                 .unwrap();
-
-        let mut consensus_parameters =
-            serde_json::from_slice::<ConsensusParameters>(CONSENSUS_PARAMETERS)
-                .expect("Invalid JSON");
-
-        chain_config.consensus_parameters = consensus_parameters.clone();
 
         let mut config = Config::local_node_with_configs(chain_config, state_config);
         config.combined_db_config.state_rewind_policy = StateRewindPolicy::RewindFullRange;
@@ -35,16 +25,16 @@ mod tests {
     }
 
     async fn start_node_and_produce_prover_input() -> anyhow::Result<Service> {
-        let metadata_path = Path::new("fixtures/testnet_block");
+        let metadata_path = Path::new("src/fixtures/testnet_block");
         // just a one-time thing
-        let db_path = Path::new("fixtures/testnet_block/db");
+        let db_path = Path::new("src/fixtures/testnet_block/db");
 
-        let fuel_node = FuelService::new_node(get_config(&metadata_path, db_path.path())).await?;
+        let fuel_node = FuelService::new_node(get_config(&metadata_path, &db_path)).await?;
 
         let service = generate_input_at_block_height(fuel_node, 1360410.into()).await?;
 
         // generate serialized input
-        let file = std::fs::File::create("fixtures/testnet_block/1360410.bin")?;
+        let file = std::fs::File::create("src/fixtures/testnet_block/1360410.bin")?;
         bincode::serialize_into(file, &service.input)?;
 
         Ok(service)
@@ -52,7 +42,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_testnet_block_1360410() {
-        let service = start_node_and_produce_prover_input().await.unwrap();
+        let _ = start_node_and_produce_prover_input().await.unwrap();
         // let serialized_input = bincode::serialize(&service.input).unwrap();
         // let proof = fuel_zkvm_primitives_prover::prove(&serialized_input).unwrap();
         // let block_id: [u8; 32] = service.input.block.header().id().into();
