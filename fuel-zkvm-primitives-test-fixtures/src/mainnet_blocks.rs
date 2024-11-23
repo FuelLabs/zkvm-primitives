c1387aec42e943916e372538bb59893558af0976
rymnc <43716372+rymnc@users.noreply.github.com>
2024-11-24 04:52:58 +0530

    fix: rename

--- fuel-zkvm-primitives-test-fixtures/src/mainnet_blocks.rs
+++ fuel-zkvm-primitives-test-fixtures/src/mainnet_blocks.rs
@@ -4,25 +4,27 @@
     feature = "enhanced_enums",
     derive(enum_iterator::Sequence, clap::ValueEnum)
 )]
+#[cfg_attr(feature = "enhanced_enums", clap(rename_all = "snake_case"))]
+#[allow(non_camel_case_types)]
 #[derive(Debug, Copy, Clone)]
 pub enum MainnetBlocks {
     // This is an empty block with just a mint tx
-    Block1522295,
+    Block_1522295,
     // This is a block with a mint tx and a contract interaction with multiple assets
-    Block2243673,
+    Block_2243673,
     // This is a block with 513 transfers
-    Block6333890,
+    Block_6333890,
 }
 
 pub fn get_mainnet_block_input(block: MainnetBlocks) -> Vec<u8> {
     match block {
-        MainnetBlocks::Block1522295 => {
+        MainnetBlocks::Block_1522295 => {
             include_bytes!("fixtures/mainnet_blocks/1522295.bin").to_vec()
         }
-        MainnetBlocks::Block2243673 => {
+        MainnetBlocks::Block_2243673 => {
             include_bytes!("fixtures/mainnet_blocks/2243673.bin").to_vec()
         }
-        MainnetBlocks::Block6333890 => {
+        MainnetBlocks::Block_6333890 => {
             include_bytes!("fixtures/mainnet_blocks/6333890.bin").to_vec()
         }
     }
@@ -37,7 +39,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_mainnet_block_1522295() {
-        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block1522295);
+        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block_1522295);
 
         let proof = prove(&serialized_input).unwrap();
         let deserialized_input: Input = bincode::deserialize(&serialized_input).unwrap();
@@ -48,7 +50,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_mainnet_block_2243673() {
-        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block2243673);
+        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block_2243673);
 
         let proof = prove(&serialized_input).unwrap();
         let deserialized_input: Input = bincode::deserialize(&serialized_input).unwrap();
@@ -59,7 +61,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_mainnet_block_6333890() {
-        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block6333890);
+        let serialized_input = get_mainnet_block_input(MainnetBlocks::Block_6333890);
 
         let proof = prove(&serialized_input).unwrap();
         let deserialized_input: Input = bincode::deserialize(&serialized_input).unwrap();
