e2cb1f538a9f1688757dacff5742f5871814ba22
rymnc <43716372+rymnc@users.noreply.github.com>
2024-11-26 12:52:59 +0530

    fix: make instruction public

--- fuel-zkvm-primitives-test-fixtures/src/opcodes.rs
+++ fuel-zkvm-primitives-test-fixtures/src/opcodes.rs
@@ -7,7 +7,7 @@ use fuel_core_storage::StorageAsRef;
 use fuel_zkvm_primitives_utils::vm::base::AsRepr;
 use fuel_zkvm_primitives_utils::vm::blob::BlobInstruction;
 use fuel_zkvm_primitives_utils::vm::other::OtherInstruction;
-use fuel_zkvm_primitives_utils::vm::Instruction;
+pub use fuel_zkvm_primitives_utils::vm::{Instruction, alu::AluInstruction};
 use fuels::prelude::Contract;
 use fuels::{accounts::Account, prelude::WalletUnlocked, types::BlockHeight};
 use fuels_core::types::transaction_builders::{
