e5da8667d5bfb5c31fd5a559e7ae6a9bb55a3b57
rymnc <43716372+rymnc@users.noreply.github.com>
2024-12-05 02:35:52 +0530

    fix: remove unused deps, regen inps, helper function exposed

--- fuel-zkvm-primitives-test-fixtures/src/fixtures.rs
+++ fuel-zkvm-primitives-test-fixtures/src/fixtures.rs
@@ -1,8 +1,9 @@
-use crate::mainnet_blocks::MainnetBlocks;
+use crate::mainnet_blocks::{get_mainnet_block_input, MainnetBlocks};
 use clap::builder::PossibleValue;
 use clap::ValueEnum;
 use fuel_zkvm_primitives_utils::vm::Instruction;
 use std::sync::OnceLock;
+use crate::opcodes::get_opcode_input;
 
 static FIXTURE_VARIANTS: OnceLock<Vec<Fixture>> = OnceLock::new();
 
@@ -15,6 +16,15 @@ pub fn all_fixtures() -> &'static Vec<Fixture> {
 pub enum Fixture {
     MainnetBlock(MainnetBlocks),
     Opcode(Instruction),
+}
+
+impl Fixture {
+    pub fn get_input_for_fixture(fixture: &Fixture) -> Vec<u8> {
+        match fixture {
+            Fixture::MainnetBlock(block) => get_mainnet_block_input(block.clone()),
+            Fixture::Opcode(instruction) => get_opcode_input(instruction.clone()),
+        }
+    }
 }
 
 impl ValueEnum for Fixture {
